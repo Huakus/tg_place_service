3cb41d294ac977efa44141456fa721410c8cd8e0
Joaquin Senn <joaquinsenn@gmail.net>
2023-07-07 13:00:04 +0200

    Dockerized application.

--- src/main/java/com/tourguide/place/model/City.java
+++ src/main/java/com/tourguide/place/model/City.java
@@ -15,9 +15,6 @@ import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
-import jakarta.validation.constraints.DecimalMax;
-import jakarta.validation.constraints.DecimalMin;
-import jakarta.validation.constraints.Digits;
 import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -40,21 +37,18 @@ public class City {
     @Column(nullable = false)
     private String name;
 
-    @DecimalMin("-90.0")
-    @DecimalMax("90.0")
-    @Digits(integer = 3, fraction = 6)
-    @Column(nullable = false, precision = 9, scale = 6)
-    private Double latitude;
+    @Column(nullable = false)
+    private String description;
 
-    @DecimalMin("-180.0")
-    @DecimalMax("180.0")
-    @Digits(integer = 4, fraction = 6)
-    @Column(nullable = false, precision = 10, scale = 6)
-    private Double longitude;
+    @Column(nullable = false)
+    private Double latitude;
+    
+    @Column(nullable = false)
+    private Double longitude;    
 
     @ManyToOne
     @JoinColumn(name = "province_id", nullable = false)
-    private Province province;
+    private Province province; 
 
     @CreatedDate
     private LocalDateTime createdAt;
--- src/main/java/com/tourguide/place/model/Place.java
+++ src/main/java/com/tourguide/place/model/Place.java
@@ -15,9 +15,6 @@ import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
-import jakarta.validation.constraints.DecimalMax;
-import jakarta.validation.constraints.DecimalMin;
-import jakarta.validation.constraints.Digits;
 import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -43,17 +40,11 @@ public class Place {
     @Column(nullable = false)
     private String description;
 
-    @DecimalMin("-90.0")
-    @DecimalMax("90.0")
-    @Digits(integer = 3, fraction = 6)
-    @Column(nullable = false, precision = 9, scale = 6)
+    @Column(nullable = false)
     private Double latitude;
-
-    @DecimalMin("-180.0")
-    @DecimalMax("180.0")
-    @Digits(integer = 4, fraction = 6)
-    @Column(nullable = false, precision = 10, scale = 6)
-    private Double longitude;
+    
+    @Column(nullable = false)
+    private Double longitude;    
 
     @ManyToOne
     @JoinColumn(name = "city_id", nullable = false)
